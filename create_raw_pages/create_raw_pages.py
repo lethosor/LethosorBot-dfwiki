d52a87c8c3580954053326a05bab417c897f0f25
lethosor <stahlalan19@gmail.com>
2013-08-07 18:56:58 -0400

    Update create_raw_pages
    Add sample creature list, implement parsing

--- create_raw_pages/create_raw_pages.py
+++ create_raw_pages/create_raw_pages.py
@@ -12,6 +12,20 @@ File format: (These values on one line, tab-separated)
 
 import wikibot
 
+templates = {
+    'std': """\
+    <noinclude>{{raw header|{% data[2] %}|{% data[4] %}}}</noinclude>\
+    {{raw|{% data[1] %}|{% data[3] %}|{% data[4] %}}}\
+    <noinclude>{{raw footer|{% data[2] %}|{% data[4] %}}}</noinclude>
+    """.replace('  ', ''),
+    'var': """\
+    <noinclude>{{raw header|{% data[2] %}|{% data[4] %}}}</noinclude>\
+    {{variation raw|{% data[1] %}|{% data[3] %}|{% data[4] %}|{{{1|}}}|\
+    {% data[5] %}}}\
+    <noinclude>{{raw footer|{% data[2] %}|{% data[4] %}}}</noinclude>
+    """.replace('  ', '')
+}
+
 class CreateRawTask(wikibot.bot.Task):
     pass
 
@@ -34,8 +48,29 @@ def run():
                 return
             else:
                 break
-        contents = f.read();
-        f.close()
+    else:
+        try:
+            f = open(args['file'])
+        except Exception as e:
+            util.log(e)
+            return
+    contents = f.read();
+    f.close()
+    contents = contents.replace('\r', '')
+    array = []
+    lines = contents.split('\n')
+    lines.remove('')
+    for line in lines:
+        array.append(line.split('\t'))
+    
+    pages = []
+    data = {}
+    for d in array:
+        pages.append(d[0])
+        template = templates['var'] if len(d) > 5 else templates['std']
+        data[d[0]] = {}
+        data[d[0]]['text'] = wikibot.util.str_format(template, data=d)
+    
 
 if __name__ == '__main__':
     run()
