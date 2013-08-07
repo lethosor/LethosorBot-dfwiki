f0f2fbd22cf0518d61be6e10bde01ca8308ad1c8
lethosor <stahlalan19@gmail.com>
2013-08-07 16:02:51 -0400

    Initial commit

---
+++ create_raw_pages.py
@@ -0,0 +1,14 @@
+"""
+Creates DF raw pages
+
+File format: (These values on one line, tab-separated)
+* Page name
+* File (with namespace)
+* Category type (creature, inorganic, etc.)
+* Object type (CREATURE, INORGANIC, etc.)
+* Object ID (e.g. BIRD_BLUEJAY)
+* Creature variation files (optional, pipe-separated) 
+"""
+
+import wikibot
+
