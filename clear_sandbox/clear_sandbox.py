b963991f4a9477a241292fdeb4621b980a3d4375
lethosor <stahlalan19@gmail.com>
2013-08-08 20:40:06 -0400

    Clear sandbox script

---
+++ clear_sandbox/clear_sandbox.py
@@ -0,0 +1,39 @@
+"""
+Clears the sandbox (DF wiki)
+
+Arguments:
+--last-line: The beginning of the last line to keep
+--sandbox: Title of sandbox; defaults to 'Project:Sandbox'
+--no-prompt: If present, do not prompt before saving
+"""
+
+import wikibot
+
+def run():
+    user = wikibot.cred.get_user()
+    args = wikibot.command_line.parse_args()
+    
+    last_line = args['last-line'] if 'last-line' in args else wikibot.util.input('Last line: ')
+    sandbox = args['sandbox'] if 'sandbox' in args else 'Project:Sandbox'
+    silent = 'silent' in args
+    
+    page = user.get_page(sandbox)
+    lines = page.text.splitlines()
+    text = ''
+    for line in lines:
+        text += line + '\n'
+        if line.startswith(last_line):
+            break
+    
+    if not silent:
+        print text
+        wikibot.util.input('\nEnter to save, Ctrl-C to abort: ')
+    page.text = text
+    page.save('Emptying sandbox', bot=1)
+    wikibot.util.log('Emptied sandbox')
+
+if __name__ == '__main__':
+    try:
+        run()
+    except KeyboardInterrupt:
+        print 'Aborted'
