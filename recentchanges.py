725c1676ba2a90243a8b92a70c464ed880d259ca
lethosor <lethosor@gmail.com>
2014-02-12 23:34:43 -0500

    Improve recentchanges display

--- recentchanges.py
+++ recentchanges.py
@@ -9,12 +9,18 @@ if not user:
     print('no user')
     sys.exit()
 os.system('clear')
+def log(text, color='black'):
+    wikibot.util.logf('<{0}>{1: <50}\r'.format(color, text))
+
 try:
     while 1:
         if len(user.api_request({'list':'recentchanges', 'rcprop':'user', 'rclimit':1, 'rcshow': '!patrolled'}, auto_filter=0)['query']['recentchanges']):
-            wikibot.util.logf('\r<green>New changes!   ')
+            log('New changes!', 'green')
         else:
-            wikibot.util.logf('\r<red>No new changes.')
+            log('No new changes.', 'red')
         time.sleep(60)
+        log('Checking...')
+except Exception as e:
+    wikibot.util.log('\n<bold,red>Error:<red> ' + str(e))
 except:
     wikibot.util.log('')
