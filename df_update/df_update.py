ac3c38323e520db5929b070c3f1f1c2ac6aa05a8
lethosor <lethosor@gmail.com>
2014-02-21 18:12:42 -0500

    Add dry run option

--- df_update/df_update.py
+++ df_update/df_update.py
@@ -2,15 +2,20 @@
 import re, time, wikibot
 util = wikibot.util
 
+wikibot.command_line.parser.add_argument('--dry-run', action='store_true', help='Dry run')
+args = wikibot.command_line.parse_args()
+
 user = wikibot.cred.get_user()
 
 version_pattern = re.compile(r'download\s+dwarf\s+fortress\s+([0-9\.]+)\s+\(([^)]+)\)', re.I)
 
 while 1:
     try:
-        req = wikibot.network.Request('http://www.bay12games.com/dwarves/')
-        version, date = version_pattern.search(req.response_text).groups()
-        #version, date = '0.34.11', 'June 4, 2012'
+        if not args.dry_run:
+            req = wikibot.network.Request('http://www.bay12games.com/dwarves/')
+            version, date = version_pattern.search(req.response_text).groups()
+        else:
+            version, date = '0.34.11', 'June 4, 2012'
     except Exception as e:
         util.log('Unable to retrieve version: %s' % e, type='warn')
     util.log('Retrieving current version...')
@@ -22,12 +27,14 @@ while 1:
     util.log('Current: %s: %s | Wiki: %s: %s' % (version, date, version_text, date_text))
     updated = False
     if version_text != version:
-        version_page.text = version + '<' + version_page.text.split('<', 1)[1]
-        version_page.save(summary=summary)
+        if not args.dry_run:
+            version_page.text = version + '<' + version_page.text.split('<', 1)[1]
+            version_page.save(summary=summary, bot=1)
         updated = True
     if date_text != date:
-        date_page.text = date + '<' + date_page.text.split('<', 1)[1]
-        date_page.save(summary=summary)
+        if not args.dry_run:
+            date_page.text = date + '<' + date_page.text.split('<', 1)[1]
+            date_page.save(summary=summary, bot=1)
         updated = True
     if updated:
         util.log('<green>New version!!!!')
