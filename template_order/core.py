05c9cc3aaa20cd7c36065640f8502260f0862fa2
lethosor <lethosor@gmail.com>
2016-08-23 10:54:11 -0400

    template_order: add diagnostics when no changes are made

--- template_order/core.py
+++ template_order/core.py
@@ -7,12 +7,19 @@ config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'cr
 site = mwclient.ex.ConfiguredSite(config_path)
 
 def run(generator, old_pattern, new_pattern, summary, dry_run=False):
+    empty = True
+    fixed = False
     for page in generator:
+        empty = False
         text = page.text()
         if old_pattern in text:
+            fixed = True
             print('{0} {1}'.format('Fixing' if not dry_run else 'Would fix', page.name))
             if dry_run:
                 continue
             text = text.replace(old_pattern, new_pattern)
             page.save(text, summary=summary, minor=True)
-
+    if empty:
+        print('No pages found in list')
+    elif not fixed:
+        print('No pages in list were fixed')
